03a580bcc1bb63adcaf2df9ef9246db43f88a252
Maij3 <jaimerodriguez123456@gmail.com>
2021-02-17 00:24:28 -0400

    mod-servicios

--- templates/gantryjnilla/html/mod_articles_category/accordion-home.php
+++ templates/gantryjnilla/html/mod_articles_category/accordion-home.php
@@ -3,6 +3,8 @@ defined('_JEXEC') or die;
 
 $items = $list;
 
+echo "<h1>PREGUNTAS FRENCUENTES SOBRE DEFENSA PENAL <span class='linea'><span> </h1>";
+
 foreach ($items as $item){
 	$accordionItems[] = array(
 		'title' => $item->title,
